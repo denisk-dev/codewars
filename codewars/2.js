fa34222c32ee3a4aa2a70cdca5724734c766d2e2
Denis Kravchenko <kravchden.b@gmail.com>
2022-08-30 23:30:44 -0400

    Update 2.js

--- codewars/2.js
+++ codewars/2.js
@@ -1,3 +1,42 @@
 function order(words) {
   // ...
+  if (!words) {
+    return "";
+  }
+
+  const dsdfsdf = words.split(" ");
+
+  const ssdfse = dsdfsdf.map((d) => {
+    arr = d.split("");
+
+    let index = null;
+
+    arr.forEach((i) => {
+      if (Number(i)) {
+        index = Number(i);
+      }
+    });
+
+    return { index, arr };
+  });
+
+  ssdfse.sort((a, b) => {
+    if (a.index > b.index) {
+      return 1;
+    }
+
+    if (a.index < b.index) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  const sdfsdfwer = ssdfse.map((rofl) => {
+    return rofl.arr.join("");
+  });
+
+  return sdfsdfwer.join(" ");
 }
+
+order("is2 Thi1s T4est 3a");
